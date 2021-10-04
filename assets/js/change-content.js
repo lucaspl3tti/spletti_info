7228857ed12f059befde2563f9ce164895d2af7a
Jan-Luca Splettstößer <jan-luca@spletti-din.de>
2021-10-04 23:52:25 +0200

    Fix styling and add new content

--- assets/js/change-content.js
+++ assets/js/change-content.js
@@ -1,47 +1,47 @@
-const skills = document.querySelector('.change-skills')
-const skillsContent = document.querySelector('.skills-content')
-const qualifications = document.querySelector('.change-qualifications')
-const qualificationsContent = document.querySelector('.qualifications-content')
+const skillsLink = document.querySelector(".change-skills");
+const skillsContent = document.querySelector(".skills-content");
+const qualificationsLink = document.querySelector(".change-qualifications");
+const qualificationsContent = document.querySelector(".qualifications-content");
 
-console.log(skills)
-console.log(skillsContent)
-console.log(qualifications)
-console.log(qualificationsContent)
-
-skills.addEventListener('click', onSkillsClickAction)
-qualifications.addEventListener('click', onQualificationsClickAction)
+skillsLink.addEventListener("click", onSkillsClickAction);
+qualificationsLink.addEventListener("click", onQualificationsClickAction);
 
 function onSkillsClickAction() {
-    if (skillsContent.style.display === 'none' && qualificationsContent.style.display != 'none') {
-        showSkills()
-        hideQualifications()
+    if (
+        skillsContent.style.display === "none" &&
+        qualificationsContent.style.display != "none"
+    ) {
+        showSkills();
+        hideQualifications();
     }
 }
 
 function onQualificationsClickAction() {
-    if (qualificationsContent.style.display === 'none' && skillsContent.style.display != 'none') {
-        showQualifications()
-        hideSkills()
+    if (
+        qualificationsContent.style.display === "none" &&
+        skillsContent.style.display != "none"
+    ) {
+        showQualifications();
+        hideSkills();
     }
 }
 
 function showSkills() {
-    skillsContent.style.display = 'flex';
-    skills.classList.add('skills-is-active')
+    skillsContent.style.display = "flex";
+    skillsLink.classList.add("skills-is-active");
 }
 
 function hideSkills() {
-    skillsContent.style.display = 'none'
-    skills.classList.remove('skills-is-active')
+    skillsContent.style.display = "none";
+    skillsLink.classList.remove("skills-is-active");
 }
 
 function showQualifications() {
-    qualificationsContent.style.display = 'flex'
-    qualifications.classList.add('qualifications-is-active')
+    qualificationsContent.style.display = "flex";
+    qualificationsLink.classList.add("qualifications-is-active");
 }
 
 function hideQualifications() {
-    qualificationsContent.style.display = 'none'
-    qualifications.classList.remove('qualifications-is-active')
-
+    qualificationsContent.style.display = "none";
+    qualificationsLink.classList.remove("qualifications-is-active");
 }
