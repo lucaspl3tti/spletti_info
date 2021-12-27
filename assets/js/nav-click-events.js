086ff2b4e0aa99c3430b8624df0907f8a0313941
Jan-Luca Splettstößer <jan-luca@spletti-din.de>
2021-12-27 14:03:41 +0100

    Fix page styling and add new qualifications container

---
+++ assets/js/nav-click-events.js
@@ -0,0 +1,67 @@
+/**
+ * This Plugin handles the custom click events for the navigation links
+ */
+export default class NavClickEventsPlugin {
+    constructor(el) {
+        // get default elements
+
+        if (document.querySelector('body').classList.contains('main-page')) {
+            this.el = document.querySelector(el);
+
+            // get nav els
+            this.portfolioLink = this.el.querySelector('.nav-portfolio');
+            this.skillsLink = this.el.querySelector('.nav-skills');
+            this.aboutLink = this.el.querySelector('.nav-about');
+            this.contactLink = this.el.querySelector('.nav-contact');
+
+            // get section els
+            this.mainSection = document.querySelector('main');
+            this.aboutEl = this.mainSection.querySelector('#about-el');
+            this.contactEl = this.mainSection.querySelector('#contact');
+
+            this.registerClickEvents();
+        }
+    }
+
+    registerClickEvents() {
+        this.scrollToPortfolio();
+        this.scrollToSkills();
+        this.scrollToAbout();
+        this.scrollToContact();
+    }
+
+    scrollToPortfolio() {
+        let pos = 480;
+
+        this.portfolioLink.addEventListener('click', () => {
+            this.scrollToPosition(pos);
+        });
+    }
+
+    scrollToSkills() {
+        let pos = 1300;
+
+        this.skillsLink.addEventListener('click', () => {
+            this.scrollToPosition(pos);
+        })
+    }
+
+    scrollToAbout() {
+        this.aboutLink.addEventListener('click', () => {
+            this.aboutEl.scrollIntoView({behavior: 'smooth'})
+        })
+    }
+
+    scrollToContact() {
+        this.contactLink.addEventListener('click', () => {
+            this.contactEl.scrollIntoView({behavior: 'smooth'})
+        })
+    }
+
+    scrollToPosition(pos) {
+        window.scrollTo({
+            top: pos,
+            behavior: 'smooth'
+        })
+    }
+}
